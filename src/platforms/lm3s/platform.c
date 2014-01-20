1653f5d87e5ceda2daa7f952da6841bb9b4506f5
Fredrik Ahlberg <fredrik@z80.se>
2014-01-20 01:30:10 +0100

    lm3s experimental

---
+++ src/platforms/lm3s/platform.c
@@ -0,0 +1,168 @@
+#include "platform.h"
+#include "gdb_if.h"
+
+#include <libopencm3/lm3s/rcc.h>
+#include <libopencm3/lm3s/nvic.h>
+#include <libopencm3/lm3s/uart.h>
+#include <libopencm3/cm3/systick.h>
+
+#define SYSTICKHZ	10
+#define SYSTICKMS	(1000 / SYSTICKHZ)
+
+#define PLL_DIV_80MHZ	5
+#define PLL_DIV_50MHZ	8
+
+jmp_buf fatal_error_jmpbuf;
+uint8_t running_status;
+volatile uint32_t timeout_counter;
+
+static void morse_update(void);
+
+/* Morse code patterns and lengths */
+static const struct {
+	uint16_t code;
+	uint8_t bits;
+} morse_letter[] = {
+	{        0b00011101,  8}, // 'A' .-
+	{    0b000101010111, 12}, // 'B' -...
+	{  0b00010111010111, 14}, // 'C' -.-.
+	{      0b0001010111, 10}, // 'D' -..
+	{            0b0001,  4}, // 'E' .
+	{    0b000101110101, 12}, // 'F' ..-.
+	{    0b000101110111, 12}, // 'G' --.
+	{      0b0001010101, 10}, // 'H' ....
+	{          0b000101,  6}, // 'I' ..
+	{0b0001110111011101, 16}, // 'J' .---
+	{    0b000111010111, 12}, // 'K' -.-
+	{    0b000101011101, 12}, // 'L' .-..
+	{      0b0001110111, 10}, // 'M' --
+	{        0b00010111,  8}, // 'N' -.
+	{  0b00011101110111, 14}, // 'O' ---
+	{  0b00010111011101, 14}, // 'P' .--.
+	{0b0001110101110111, 16}, // 'Q' --.-
+	{      0b0001011101, 10}, // 'R' .-.
+	{        0b00010101,  8}, // 'S' ...
+	{          0b000111,  6}, // 'T' -
+	{      0b0001110101, 10}, // 'U' ..-
+	{    0b000111010101, 12}, // 'V' ...-
+	{    0b000111011101, 12}, // 'W' .--
+	{  0b00011101010111, 14}, // 'X' -..-
+	{0b0001110111010111, 16}, // 'Y' -.--
+	{  0b00010101110111, 14}, // 'Z' --..
+};
+
+const char *morse_msg;
+static const char * volatile morse_ptr;
+static char morse_repeat;
+
+void morse(const char *msg, char repeat)
+{
+	morse_msg = morse_ptr = msg;
+	morse_repeat = repeat;
+	SET_ERROR_STATE(0);
+}
+
+static void morse_update(void)
+{
+	static uint16_t code;
+	static uint8_t bits;
+
+	if(!morse_ptr) return;
+
+	if(!bits) {
+		char c = *morse_ptr++;
+		if(!c) {
+			if(morse_repeat) {
+				morse_ptr = morse_msg;
+				c = *morse_ptr++;
+			} else {
+				morse_ptr = 0;
+				return;
+			}
+		}
+		if((c >= 'A') && (c <= 'Z')) {
+			c -= 'A';
+			code = morse_letter[c].code;
+			bits = morse_letter[c].bits;
+		} else {
+			code = 0; bits = 4;
+		}
+	}
+	SET_ERROR_STATE(code & 1);
+	code >>= 1; bits--;
+}
+
+
+void sys_tick_handler(void)
+{
+	if(running_status) {
+		gpio_toggle(LED_PORT, LED_IDLE_RUN);
+	}
+
+	if(timeout_counter)
+		timeout_counter--;
+
+	morse_update();
+}
+
+int platform_init(void)
+{
+        int i;
+        for(i=0; i<1000000; i++);
+
+	rcc_sysclk_config(OSCSRC_MOSC, XTAL_8M, PLL_DIV_50MHZ);
+	
+	// Enable LED
+	periph_clock_enable(RCC_GPIOD);
+	__asm__("nop"); __asm__("nop"); __asm__("nop");
+	gpio_mode_setup(LED_PORT, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, LED_IDLE_RUN);
+	gpio_set_output_config(LED_PORT, GPIO_OTYPE_PP, GPIO_DRIVE_2MA, LED_IDLE_RUN);
+	//gpio_mode_setup(LED_PORT, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, LED_ERROR);
+	//gpio_set_output_config(LED_PORT, GPIO_OTYPE_PP, GPIO_DRIVE_2MA, LED_ERROR);
+
+	// Enable all JTAG ports and set pins to output
+	periph_clock_enable(RCC_GPIOA);
+	periph_clock_enable(RCC_GPIOB);
+	periph_clock_enable(RCC_GPIOE);
+	__asm__("nop"); __asm__("nop"); __asm__("nop");
+
+	gpio_mode_setup(TMS_PORT, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, TMS_PIN);
+	gpio_mode_setup(TCK_PORT, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, TCK_PIN);
+	gpio_mode_setup(TDI_PORT, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, TDI_PIN);
+	gpio_mode_setup(TDO_PORT, GPIO_MODE_INPUT, GPIO_PUPD_NONE, TDO_PIN);
+	gpio_mode_setup(TRST_PORT, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, TRST_PIN);
+	gpio_mode_setup(SRST_PORT, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, SRST_PIN);
+
+	// Stellaris processors don't have the DIV8 clock source.
+	systick_set_clocksource(STK_CSR_CLKSOURCE_AHB);
+	systick_set_reload(rcc_get_system_clock_frequency() / (SYSTICKHZ));
+
+	systick_interrupt_enable();
+	systick_counter_enable();
+
+	nvic_enable_irq(NVIC_SYSTICK_IRQ);
+	//nvic_enable_irq(NVIC_UART0_IRQ);
+
+	usbuart_init();
+	cdcacm_init();
+
+	jtag_scan(NULL);
+
+	return 0;
+}
+
+void platform_delay(uint32_t delay)
+{
+	timeout_counter = delay;
+	while(timeout_counter);
+}
+
+const char *platform_target_voltage(void)
+{
+	return "not supported";
+}
+
+void assert_boot_pin(void)
+{
+	// TODO
+}
---
+++ src/platforms/lm3s/platform.h
@@ -0,0 +1,122 @@
+#ifndef __PLATFORM_H
+#define __PLATFORM_H
+
+#include <stdint.h>
+
+#include <setjmp.h>
+#include <alloca.h>
+
+#include <libopencm3/lm3s/gpio.h>
+#include <libopencm3/usb/usbd.h>
+
+#include "gdb_packet.h"
+
+#define CDCACM_PACKET_SIZE 	64
+#define BOARD_IDENT             "Black Magic Probe (LM3S), (Firmware 1.5" VERSION_SUFFIX ", build " BUILDDATE ")"
+#define BOARD_IDENT_DFU		"Black Magic (Upgrade) for LM3S, (Firmware 1.5" VERSION_SUFFIX ", build " BUILDDATE ")"
+#define DFU_IDENT               "Black Magic Firmware Upgrade (LM3S)"
+#define DFU_IFACE_STRING	"lolwut"
+
+extern usbd_device *usbdev;
+#define CDCACM_GDB_ENDPOINT	1
+#define CDCACM_UART_ENDPOINT	3
+
+extern jmp_buf fatal_error_jmpbuf;
+extern uint8_t running_status;
+extern const char *morse_msg;
+extern volatile uint32_t timeout_counter;
+
+#define LED_PORT	GPIOD_APB_BASE
+#define LED_IDLE_RUN	GPIO2
+//#define LED_ERROR	GPIO3
+
+#define TMS_PORT	GPIOB_APB_BASE
+#define TMS_PIN		GPIO5
+
+#define TCK_PORT	GPIOB_APB_BASE
+#define TCK_PIN		GPIO0
+
+#define TDI_PORT	GPIOB_APB_BASE
+#define TDI_PIN		GPIO1
+
+#define TDO_PORT	GPIOE_APB_BASE
+#define TDO_PIN		GPIO4
+
+#define SWDIO_PORT	TMS_PORT
+#define SWDIO_PIN	TMS_PIN
+
+#define SWCLK_PORT	TCK_PORT
+#define SWCLK_PIN	TCK_PIN
+
+#define TRST_PORT	GPIOA_APB_BASE
+#define TRST_PIN	GPIO5
+
+#define SRST_PORT	GPIOA_APB_BASE
+#define SRST_PIN	GPIO6
+
+#define TMS_SET_MODE()	{								\
+	gpio_mode_setup(TMS_PORT, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, TMS_PIN);		\
+	gpio_set_output_config(TMS_PORT, GPIO_OTYPE_PP, GPIO_DRIVE_2MA, TMS_PIN);	\
+}
+
+#define SWDIO_MODE_FLOAT() {								\
+	gpio_mode_setup(SWDIO_PORT, GPIO_MODE_INPUT, GPIO_PUPD_NONE, SWDIO_PIN);	\
+}
+
+#define SWDIO_MODE_DRIVE() {									\
+	gpio_mode_setup(SWDIO_PORT, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, SWDIO_PIN);		\
+	gpio_set_output_config(SWDIO_PORT, GPIO_OTYPE_PP, GPIO_DRIVE_2MA, SWDIO_PIN);		\
+}
+
+extern usbd_driver lm3s_usb_driver;
+#define USB_DRIVER	lm3s_usb_driver
+#define USB_IRQ		NVIC_USB0_IRQ
+#define USB_ISR		usb0_isr
+
+#define IRQ_PRI_USB	(2 << 4)
+
+/* Use newlib provided integer only stdio functions */
+#define sscanf siscanf
+#define sprintf siprintf
+#define vasprintf vasiprintf
+
+#define DEBUG(...)
+
+#define SET_RUN_STATE(state)	{running_status = (state);}
+#define SET_IDLE_STATE(state)	{gpio_set_val(LED_PORT, LED_IDLE_RUN, state);}
+//#define SET_ERROR_STATE(state)	{} //{gpio_set_val(LED_PORT, LED_ERROR, state);}
+#define SET_ERROR_STATE(state)	SET_IDLE_STATE(state)
+
+#define PLATFORM_SET_FATAL_ERROR_RECOVERY()	{setjmp(fatal_error_jmpbuf);}
+#define PLATFORM_FATAL_ERROR(error) {			\
+	if( running_status ) gdb_putpacketz("X1D");	\
+		else gdb_putpacketz("EFF");		\
+	running_status = 0;				\
+	target_list_free();				\
+	morse("TARGET LOST.", 1);			\
+	longjmp(fatal_error_jmpbuf, (error));		\
+}
+
+int platform_init(void);
+void morse(const char *msg, char repeat);
+
+inline static void gpio_set_val(uint32_t port, uint8_t pin, uint8_t val) {
+	gpio_write(port, pin, val == 0 ? 0 : 0xff);
+}
+
+inline static uint8_t gpio_get(uint32_t port, uint8_t pin) {
+	return !(gpio_read(port, pin) == 0);
+}
+
+void platform_delay(uint32_t delay);
+
+/* <cdcacm.c> */
+void cdcacm_init(void);
+/* Returns current usb configuration, or 0 if not configured. */
+int cdcacm_get_config(void);
+int cdcacm_get_dtr(void);
+
+#define disconnect_usb() do { usbd_disconnect(usbdev,1); nvic_disable_irq(USB_IRQ);} while(0)
+#define setup_vbus_irq()
+
+#endif
